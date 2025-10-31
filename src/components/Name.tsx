558ac02b9a09712ddcc8e61c767f5848a5f1f8fc
Dat Vu <datvu@MacBook-Pro-cua-Dat.local>
2025-10-31 18:20:51 +0700

    update

--- src/components/Name.tsx
+++ src/components/Name.tsx
@@ -6,14 +6,15 @@ import * as React from "react";
 export default function Name() {
   const searchParams = useSearchParams();
   const name = searchParams.get("name");
+  console.log(name);
 
   return name ? (
     <div
       style={{ animationDelay: `2.5s`, textShadow: "2px 2px 7px #000" }}
       className="font-the-nautigal right-to-left text-[40px] md:text-[8svh] text-shadow-lg font-[700]"
     >
-      <span className="text-[40px]">
-        Trân trọng kính mời: {name.length > 14 && <br />}
+      <span className="text-[40px] text-center">
+        Trân trọng mời: {name.length > 14 && <br />}
       </span>{" "}
       {name.replaceAll("-", " ")}
     </div>
