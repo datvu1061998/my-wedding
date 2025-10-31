9eba980b9ec06e90befa2bfb334040a20a1ff410
Dat Vu <datvu@MacBook-Pro-cua-Dat.local>
2025-10-31 18:13:55 +0700

    update

--- src/components/Name.tsx
+++ src/components/Name.tsx
@@ -12,7 +12,10 @@ export default function Name() {
       style={{ animationDelay: `2.5s`, textShadow: "2px 2px 7px #000" }}
       className="font-the-nautigal right-to-left text-[40px] md:text-[8svh] text-shadow-lg font-[700]"
     >
-      <span className="text-[40px]">Trân trọng kính mời:</span> {name}
+      <span className="text-[40px]">
+        Trân trọng kính mời: {name.length > 14 && <br />}
+      </span>{" "}
+      {name.replaceAll("-", " ")}
     </div>
   ) : (
     ""
